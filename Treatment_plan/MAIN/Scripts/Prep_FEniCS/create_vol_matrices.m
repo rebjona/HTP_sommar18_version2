ee6f822a39c11a2c9fa331f476b8f270a5e4b3fb
Rebecca Jonasson <rebeccajonasson@Rebeccas-MacBook-Air.local>
2018-06-19 09:10:54 +0200

    Prep fenics

--- Treatment_plan/MAIN/Scripts/Prep_FEniCS/create_vol_matrices.m
+++ Treatment_plan/MAIN/Scripts/Prep_FEniCS/create_vol_matrices.m
@@ -1,9 +1,9 @@
-function create_vol_matrices(overwriteOutput, tissue_mat, thermal_conductivity, perf_cap, density, heat_capacity modelType)
+function create_vol_matrices(overwriteOutput, tissue_mat, thermal_conductivity, perf_cap, density, heat_capacity, modelType)
 %CREATE_VOL_MATRICES(overwriteOutput, thermal_conductivity, perf_cap, tissue_mat)
 %   Creates and saves volumetric (non-boundary) parameter matrices. 
 %   Input: lists of material properties for each tissue index.
 
-    if nargin ~= 5
+    if nargin ~= 7
             error('Needs five input arguments.')
     end
 
@@ -19,12 +19,12 @@ function create_vol_matrices(overwriteOutput, tissue_mat, thermal_conductivity, 
     
     if ~exist(get_path('density_mat'),'var' ) || overwriteOutput 
         density_mat=density(tissue_mat);
-        save(get_path('thermal_cond_mat'), 'density_mat', '-v7.3');
+        save(get_path('density_mat'), 'density_mat', '-v7.3');
     end
     
-    if ~exist(get_path('thermal_capacity_mat'),'var') || overwriteOutput
+    if ~exist(get_path('heat_capacity_mat'),'var') || overwriteOutput
         thermal_capacity_mat=thermal_capacity(tissue_mat);
-        save(get_path('thermal_cond_mat'), 'thermal_capacity_mat', '-v7.3'); 
+        save(get_path('heat_capacity_mat'), 'thermal_capacity_mat', '-v7.3'); 
     end    
         
     if ~exist(get_path('perfusion_heatcapacity_mat'),'var') || overwriteOutput
